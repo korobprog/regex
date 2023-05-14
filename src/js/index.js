12a21824a69580e6e091a76bb9dbeceab41fb693
korobprog <korobprog@gmail.com>
2023-05-14 20:40:28 +1000

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+export default class Validator {
+  static validateUsername(username) {
+    const regex = /^(?=.{3,20}$)(?![.\d_-])(?!.*[.\d_-]{2})[a-zA-Z0-9._-]+(?<![.\d_-])$/;
+    return regex.test(username);
+  }
+}
