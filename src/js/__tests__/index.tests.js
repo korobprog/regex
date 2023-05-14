088d8d1e64dd31035dcfc846ee33171b3ab0a968
korobprog <korobprog@gmail.com>
2023-05-15 05:53:33 +1000

    fix eslint

--- src/js/__tests__/index.tests.js
+++ src/js/__tests__/index.tests.js
@@ -1,30 +1,32 @@
-import Validator from '../index';
+import Validator from "../index";
 
-describe('Validator', () => {
-  describe('validateUsername', () => {
-    it('should return false for username containing invalid characters', () => {
-      expect(Validator.validateUsername('user@name')).toBe(false);
-      expect(Validator.validateUsername('myusername#')).toBe(false);
+describe("Validator", () => {
+  describe("validateUsername", () => {
+    it("should return false for username containing invalid characters", () => {
+      expect(Validator.validateUsername("user@name")).toBe(false);
+      expect(Validator.validateUsername("myusername#")).toBe(false);
     });
 
-    it('should return false for username that is too short or too long', () => {
-      expect(Validator.validateUsername('a')).toBe(false);
-      expect(Validator.validateUsername('this_username_is_way_too_long')).toBe(false);
+    it("should return false for username that is too short or too long", () => {
+      expect(Validator.validateUsername("a")).toBe(false);
+      expect(Validator.validateUsername("this_username_is_way_too_long")).toBe(
+        false
+      );
     });
 
-    it('should return false for username starting or ending with invalid characters', () => {
-      expect(Validator.validateUsername('.username')).toBe(false);
-      expect(Validator.validateUsername('_username')).toBe(false);
-      expect(Validator.validateUsername('-username')).toBe(false);
-      expect(Validator.validateUsername('username.')).toBe(false);
-      expect(Validator.validateUsername('username_')).toBe(false);
-      expect(Validator.validateUsername('username-')).toBe(false);
+    it("should return false for username starting or ending with invalid characters", () => {
+      expect(Validator.validateUsername(".username")).toBe(false);
+      expect(Validator.validateUsername("_username")).toBe(false);
+      expect(Validator.validateUsername("-username")).toBe(false);
+      expect(Validator.validateUsername("username.")).toBe(false);
+      expect(Validator.validateUsername("username_")).toBe(false);
+      expect(Validator.validateUsername("username-")).toBe(false);
     });
 
-    it('should return false for username containing consecutive dots, underscores, or hyphens', () => {
-      expect(Validator.validateUsername('user..name')).toBe(false);
-      expect(Validator.validateUsername('myusername__')).toBe(false);
-      expect(Validator.validateUsername('myusername--')).toBe(false);
+    it("should return false for username containing consecutive dots, underscores, or hyphens", () => {
+      expect(Validator.validateUsername("user..name")).toBe(false);
+      expect(Validator.validateUsername("myusername__")).toBe(false);
+      expect(Validator.validateUsername("myusername--")).toBe(false);
     });
   });
 });
