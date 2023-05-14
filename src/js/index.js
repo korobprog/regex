b5c383b424f01cb7997f37cef832311aad0d743b
korobprog <korobprog@gmail.com>
2023-05-15 05:43:07 +1000

    fix parser eslint

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
-export default class Validator {
+class Validator {
   static validateUsername(username) {
-    const regex = /^(?=.{3,20}$)(?![.\d_-])(?!.*[.\d_-]{2})[a-zA-Z0-9._-]+(?<![.\d_-])$/;
+    const regex =
+      /^(?=.{3,20}$)(?![.\d_-])(?!.*[.\d_-]{2})[a-zA-Z0-9._-]+(?<![.\d_-])$/;
     return regex.test(username);
   }
 }
+
+export default Validator;
